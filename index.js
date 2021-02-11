2bed831cd76b60fb411e1162650fd667da41a292
pamela-almeida <76069371+pamela-almeida@users.noreply.github.com>
2021-02-11 17:28:22 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function Click() {
+var A= "oito"
+console.log (A)
+var resposta= confirm (A)
+if(resposta) {document.getElementById("input").value= "você conconrdou"}
+else {document.getElementById("input").value= "você não concordou"}
+
+
+}
